e1bfb48aea26c3eed07cb6dc910d585a9b067b4e
Patricia Costa <patricosouza@hotmail.com>
2024-06-03 09:56:04 -0300

    eslint_changes

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -3,7 +3,7 @@ import { Montserrat } from 'next/font/google'
 const montserrat = Montserrat({
   subsets: ['latin'],
   variable: '--font-montserrat',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 })
 
 export const fonts = {
