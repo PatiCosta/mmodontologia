2207c135677076e0a078890dcd80d1ac30c76584
chouzzy <mathfernando@hotmail.com>
2026-04-29 15:25:50 -0300

    gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,20 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* <!-- Google Tag Manager --> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TV52PZXK');
+            `,
+          }}
+        />
+        {/* <!-- End Google Tag Manager --> */}
+        
         {/* <!-- HTML Meta Tags --> */}
         <meta name="description" content="Realçando a beleza que já existe em você." />
         <meta name="keywords" content="Prótese Dentária, Endodontia, Implantodontia, Cirurgia oral menor, Ortodontia" />
@@ -29,6 +43,13 @@ export default function Document() {
         <link rel="shortcut icon" href="/assets/logo-icon.png" />
       </Head>
       <body>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TV52PZXK" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <Main />
         <NextScript />
       </body>
